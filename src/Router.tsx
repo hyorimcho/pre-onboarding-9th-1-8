789c2ae562cec86fec385f7f8f5dc62540ea4629
hyorimm <hyorimm@gmail.com>
2023-02-23 10:06:12 +0900

    feat: 라우터 경로 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,19 @@
-const Router = () => {
-  return <div>Router</div>;
-};
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Todo from './pages/Todo';
 
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default Router;
