65d20b615633430b232dcb727928e692b9f4a675
hyorimm <hyorimm@gmail.com>
2023-02-23 15:00:49 +0900

    feat: 라우터 재설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Todo from './pages/Todo';
 
 const Router = () => {
   return (
@@ -11,7 +10,6 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
-        <Route path="todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
