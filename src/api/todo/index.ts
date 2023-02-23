820d42019993f8394281b2a8137f30ac723031f0
hyorimm <hyorimm@gmail.com>
2023-02-24 03:21:04 +0900

    feat: 투두 수정, 삭제 구현

--- src/api/todo/index.ts
+++ src/api/todo/index.ts
@@ -1,7 +1,7 @@
 import apiClient from '../apiClient';
-import { createTodoParam } from './param';
+import { CreateTodoType } from './types';
 
-export const createTodo = async ({ todo }: createTodoParam) => {
+export const createTodo = async (todo: string) => {
   return await apiClient({
     method: 'post',
     url: '/todos',
@@ -15,5 +15,27 @@ export const getTodo = async () => {
   return await apiClient({
     method: 'get',
     url: '/todos',
+  });
+};
+
+export const deleteTodo = async (id: number) => {
+  return await apiClient({
+    method: 'delete',
+    url: `/todos/${id}`,
+  });
+};
+
+export const updateTodo = async (
+  id: number,
+  todo: string,
+  isCompleted: boolean,
+) => {
+  return await apiClient({
+    method: 'put',
+    url: `/todos/${id}`,
+    data: {
+      todo,
+      isCompleted,
+    },
   });
 };
--- src/api/todo/param.ts
+++
@@ -1,3 +0,0 @@
-export interface createTodoParam {
-  todo: string;
-}
